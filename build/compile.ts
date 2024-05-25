7379e1bac779522d60553540bb18ecd73599abd7
Juyeong Maing <mjy9088@naver.com>
2024-05-26 04:24:02 +0900

    fix MDX problem

--- build/compile.ts
+++ build/compile.ts
@@ -5,6 +5,20 @@ import babel from "@babel/core";
 import { dirname } from "node:path";
 import { babelPluginSyntaxMdx } from "./plugin";
 
+const original = `export default function MDXContent(props = {}) {
+  const {
+    wrapper: MDXLayout
+  } = props.components || {};
+  return MDXLayout ? _jsx(MDXLayout, {
+    ...props,
+    children: _jsx(_createMdxContent, {
+      ...props
+    })
+  }) : _createMdxContent(props);
+}`;
+
+const replacement = `export { _createMdxContent as default };`;
+
 export async function compile(
   inFilePath: string,
   outFilePath: string
@@ -14,6 +28,14 @@ export async function compile(
     filename: inFilePath,
     plugins: [babelPluginSyntaxMdx],
   });
+  const code = result!.code!;
+
+  if (!code.endsWith(original)) {
+    throw new Error("Dependencies updated, build script must be updated.");
+  }
+  const replacedCode =
+    code.substring(0, code.length - original.length) + replacement;
+
   await mkdir(dirname(outFilePath), { recursive: true });
-  await writeFile(outFilePath, result?.code!); //NOSONAR
+  await writeFile(outFilePath, replacedCode);
 }
